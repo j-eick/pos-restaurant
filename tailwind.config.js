4f898708e7f831a1686209507c45dacf0ec046bf
Joschka Eickhoff <jeickhoff@posteo.de>
2024-04-25 11:07:15 +0200

    tailwind.config: clean up; new colors, variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,7 @@
-// import type { Config } from "tailwindcss";
-
-// const { gray } = require("tailwindcss/colors");
-
-// console.log(gray);
-
-// const config: Config = {
-//   content: [
-//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       backgroundImage: {
-//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-//         "gradient-conic":
-//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-//       },
-//       colors: {
-//         primary: gray[900],
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
-// export default config;
-
-const { gray } = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
+
+const { gray, red } = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
@@ -40,9 +14,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        //---Regular Fontcolors
         primary: gray[900],
         secondary: gray[700],
         tertiary: gray[400],
+      },
+      backgroundColor: {
+        //---Button BackgroundColors
+        red: red[300],
+        "red-hover": red[400],
+      },
+    },
+    variants: {
+      extend: {
+        scale: ["hover"],
       },
     },
   },

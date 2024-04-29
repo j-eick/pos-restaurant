fd39a0baec35623301e5d22ed6afb65a8a998b36
Joschka Eickhoff <jeickhoff@posteo.de>
2024-04-29 12:52:03 +0200

    created / Dish component

---
+++ app/components/ui/dish/Dish.tsx
@@ -0,0 +1,75 @@
+import { Text } from "../text";
+import { IoRestaurantOutline } from "react-icons/io5";
+import { LuConciergeBell } from "react-icons/lu";
+
+type MenuItemProp = {
+  dish: {
+    id: string;
+    title: string;
+    photo: string;
+    gallery: string[];
+    description: string;
+    ingredients: string[];
+    allergens: string[];
+    altText: string;
+    price: string;
+  };
+};
+
+export function Dish({ dish }: MenuItemProp) {
+  return (
+    <article className="relative m-2 pt-48 pb-5 px-5 min-h-96 rounded-xl shadow-dishCard">
+      <div className="mb-5 flex gap-2 justify-center">
+        <div>
+          {dish.gallery.length === 0 ? (
+            <div className="absolute p-[3px] h-52 w-10/12 right-1/2 translate-x-1/2 top-[-1.5rem] bg-gray-100 rounded-full">
+              <img src={dish.photo} alt={dish.altText} className="shadow-dishPhoto rounded-full " />
+            </div>
+          ) : (
+            <div className="flex flex-col gap-2">
+              <div className="absolute p-[3px] h-52 w-10/12 right-1/2 translate-x-1/2 top-[-1.5rem] bg-gray-100 rounded-full">
+                <img src={dish.photo} alt={dish.altText} className="shadow-dishPhoto rounded-full" />
+              </div>
+              <ul role="list" className="flex gap-3 overflow-x-auto max-h-[220px] hideScrollbar">
+                {dish.gallery.map((photo, i) => (
+                  <li key={i}>
+                    <img src={photo} alt="" />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+      <div>
+        <section className="mb-7">
+          <Text tag="h1" size="lg" weight="semibold" className="mb-3">
+            {dish.title}
+          </Text>
+          <Text tag="p" size="sm">
+            {dish.description}
+          </Text>
+          <Text tag="p" size="sm" className="hidden">
+            Ingredients: {dish.ingredients}
+          </Text>
+        </section>
+        <section className="flex justify-around">
+          <div className="flex flex-col w-2/4">
+            <Text tag={"p"} size="xs" weight="medium" className="text-start">
+              Allergens
+            </Text>
+            <Text tag={"p"} size="xs" weight="light" className="text-start">
+              {dish.allergens}
+            </Text>
+          </div>
+          <div className="flex items-center justify-center w-2/4 gap-3 shadow-dishOrder rounded-lg">
+            <Text tag="p" weight="semibold">
+              &euro; {dish.price}
+            </Text>
+            <LuConciergeBell className="scale-[130%]" />
+          </div>
+        </section>
+      </div>
+    </article>
+  );
+}
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,36 +1,20 @@
 import { menuItems } from "../lib/menu";
 import { Text } from "../components/ui/text";
-import Image from "next/image";
+import { Dish } from "../components/ui/dish";
 
 export default function Menu() {
+  // useEffect(() => {}, []);
+
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-between py-24 px-10 ">
-      <Text tag="h1" size="2xl" weight="semibold">
+    <main className="relative flex min-h-screen flex-col items-center justify-between pb-24 pt-10 px-6">
+      <Text tag="h1" size="2xl" weight="semibold" className="mb-14">
         Menu
       </Text>
       <section className="text-center">
-        <ul role="list" className="flex flex-col gap-10">
+        <ul role="list" className="flex flex-col gap-14">
           {menuItems.map((item) => (
-            <li key={item.id} className="border-2 border-red-400">
-              <article>
-                <div className="flex gap-2 justify-center border-2 border-green-400 max-h-[220px]">
-                  <img src={item.photo} alt={item.altText} />
-                  <ul role="list" className="flex flex-col gap-3 overflow-y-auto max-h-[220px] hideScrollbar">
-                    {item.gallery.map((photo, i) => (
-                      <li key={i}>
-                        <img src={photo} alt="" />
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <Text tag="h1" size="lg" weight="semibold">
-                  {item.title}
-                </Text>
-
-                <Text tag="p">{item.description}</Text>
-                <Text tag="p">Ingredients: {item.ingredients}</Text>
-                <Text tag="p">Allergens: {item.allergens}</Text>
-              </article>
+            <li key={item.id}>
+              <Dish dish={item} />
             </li>
           ))}
         </ul>

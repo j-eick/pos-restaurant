d4d7762352671e3741a6f35769e0735bb0f8c16c
Joschka Eickhoff <jeickhoff@posteo.de>
2024-04-30 02:40:32 +0200

    add( Button.tsx): new component

--- app/components/ui/button/Button.tsx
+++ app/components/ui/button/Button.tsx
@@ -1,24 +1,47 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useMemo } from "react";
 import { twMerge } from "tailwind-merge";
 
 type ButtonProp = {
   children: ReactNode;
-  variant: "menuNav";
+  size: string;
+  color?: "peach" | "green";
   className?: string;
   href?: "/menu" | "/order-summary";
+  onClick?: () => void;
 };
 
-export const Button = ({ children, variant, className }: ButtonProp) => {
+export const Button = ({ children, size = "md", color, onClick, className }: ButtonProp) => {
   const hover = {
     red: `transition-all hover:bg-red-hover`,
   };
 
-  const styles = {
-    menuNav: `px-5 py-5 bg-red rounded-xl cursor-pointer ${hover.red}`,
-  };
+  const buttonSize = useMemo(() => {
+    switch (size) {
+      case "sm":
+        return "";
+      case "md":
+        return "w-16 h-16";
+      case "lg":
+        return "w-[4.5rem] h-[4.5rem]";
+
+      default:
+        return "p-3 border-2";
+    }
+  }, []);
+
+  const buttonColor = useMemo(() => {
+    switch (size) {
+      case "peach":
+        return "bg-custom-peach";
+      case "green":
+        return "custom-green";
+
+      default:
+    }
+  }, []);
 
   return (
-    <button className={twMerge(`${styles[variant]} ${className}`)}>
+    <button className={twMerge(`${buttonSize} ${buttonColor} ${className}`)} onClick={onClick}>
       {children}
     </button>
   );

681c3ed47faf8741980ac9285ec64776372b7546
Joschka Eickhoff <jeickhoff@posteo.de>
2024-04-30 19:53:54 +0200

    update

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,37 +1,43 @@
-"use client";
-
 import { menuItems } from "../lib/menu";
 import { Text } from "../components/ui/text";
 import { Dish } from "../components/ui/dish";
 import { Button } from "../components/ui/button";
 import { useState } from "react";
+import { SideMenu } from "../components/ui/sideMenu";
 
 export default function Menu() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-between pb-12 pt-10 px-6">
-      <Text tag="h1" size="2xl" weight="semibold" className="mb-14">
-        Menu
-      </Text>
-      <section className="text-center">
-        <ul role="list" className="flex flex-col gap-14">
-          {menuItems.map((item) => (
-            <li key={item.id}>
-              <Dish dish={item} />
-            </li>
-          ))}
-        </ul>
-      </section>
-      <Button
-        size="lg"
-        color="peach"
-        className="fixed z-50 bottom-6 right-6 flex justify-center items-center 
-          rounded-full bg-custom-peach"
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-      >
-        <img src="menu.png" alt="menu button" className="scale-[45%]" />
-      </Button>
+    <main className="relative">
+      <SideMenu slide={`translate-x-[${isMenuOpen ? "0" : "-100%"}]`} />
+      <div className="pt-20 pb-20 px-6 flex flex-col items-center">
+        <Text tag="h1" size="2xl" weight="semibold" className="mb-14">
+          Menu
+        </Text>
+        <section className="text-center">
+          <ul role="list" className="flex flex-col gap-20">
+            {menuItems.map((item) => (
+              <li key={item.id}>
+                <Dish dish={item} />
+              </li>
+            ))}
+          </ul>
+        </section>
+        <Button
+          size="lg"
+          color="peach"
+          className={`fixed z-50 flex justify-center items-center rounded-full transition-all duration-500
+                    ${
+                      isMenuOpen
+                        ? "bg-custom-peach scale-75 right-3 bottom-3"
+                        : "bottom-6 right-6 bg-orange-300 border-2 border-slate-100"
+                    }`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <img src="menu.png" alt="menu button" className="scale-[45%]" />
+        </Button>
+      </div>
     </main>
   );
 }

dbe48713284e9c7c60a224c8ee41167d510c9007
Joschka Eickhoff <jeickhoff@posteo.de>
2024-04-30 02:43:01 +0200

    adding Button to menu page

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import { menuItems } from "../lib/menu";
 import { Text } from "../components/ui/text";
 import { Dish } from "../components/ui/dish";
+import { Button } from "../components/ui/button";
+import { useState } from "react";
 
 export default function Menu() {
-  // useEffect(() => {}, []);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-between pb-12 pt-10 px-6">
@@ -19,6 +23,15 @@ export default function Menu() {
           ))}
         </ul>
       </section>
+      <Button
+        size="lg"
+        color="peach"
+        className="fixed z-50 bottom-6 right-6 flex justify-center items-center 
+          rounded-full bg-custom-peach"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <img src="menu.png" alt="menu button" className="scale-[45%]" />
+      </Button>
     </main>
   );
 }

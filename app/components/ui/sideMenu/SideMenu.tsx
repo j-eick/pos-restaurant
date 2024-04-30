e94f6c5bbbfbaa1232e47210bda6806ef3df7065
Joschka Eickhoff <jeickhoff@posteo.de>
2024-05-01 00:24:20 +0200

    fix

--- app/components/ui/sideMenu/SideMenu.tsx
+++ app/components/ui/sideMenu/SideMenu.tsx
@@ -9,7 +9,7 @@ export const SideMenu = ({ isOpen }: SideMenuProps) => {
   const show = "translate-x-0 transition-all duration-300 ease-in-out";
   const hide = "translate-x-[-100%] transition-all duration-300 ease-in-out";
 
-  const openClose = `fixed z-50 top-1/2 -translate-y-[45%] w-7/12 h-6/12
+  const openClose = `fixed z-50 top-1/2 -translate-y-[45%] w-7/12 h-4/5
       rounded-r-2xl pt-20 pb-20 pl-6 backdrop-blur-lg bg-opacity-60 bg-slate-300 
       border-2 border-t-app-primary border-r-app-primary border-b-app-primary
       ${isOpen ? hide : show}`;

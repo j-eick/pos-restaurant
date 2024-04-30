18cfc272fe950cab75bdee1dce2250e7ec3b7e2c
Joschka Eickhoff <jeickhoff@posteo.de>
2024-04-30 19:52:31 +0200

    added ( SideMenu ): new Component

---
+++ app/components/ui/sideMenu/SideMenu.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { menuItems } from "@/app/lib/menu";
+import { Text } from "../text/Text";
+import { useEffect, useState } from "react";
+
+type SideMenuProps = {
+  slide?: string;
+};
+
+export const SideMenu = ({ slide }: SideMenuProps) => {
+  useEffect(() => {
+    console.log(slide);
+  }, [slide]);
+
+  return (
+    <aside
+      className={`fixed z-50 top-1/2 -translate-y-[45%] w-7/12 h-4/6
+      rounded-r-2xl pt-20 pb-20 pl-6 backdrop-blur-lg bg-opacity-60 bg-slate-300 
+      border-2 border-t-app-primary border-r-app-primary border-b-app-primary 
+      transition-all duration-300 ease-in-out ${slide}`}
+    >
+      <div className="relative h-full">
+        <div
+          className="absolute top-[-10px] bottom-[-10px]
+                        left-0 border-l-2 border-slate-400"
+        />
+        <ul className="h-full flex flex-col gap-3 pl-3 overflow-auto">
+          {menuItems.map((item) => (
+            <li key={item.id}>
+              <Text tag="p" size="regular">
+                {item.title}
+              </Text>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </aside>
+  );
+};

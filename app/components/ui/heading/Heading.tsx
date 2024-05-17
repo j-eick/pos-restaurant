334979c768082a614663ebde979024d1e7787ed4
Joschka Eickhoff <jeickhoff@posteo.de>
2024-05-17 15:20:19 +0200

    added links to heading

--- app/components/ui/heading/Heading.tsx
+++ app/components/ui/heading/Heading.tsx
@@ -9,16 +9,16 @@ export function Heading() {
     <header className="fixed z-50 w-full bg-white">
       <ul role="list" className="h-16 flex justify-around">
         <li className="relative h-full w-2/6 flex justify-center">
-          <div className="absolute top-1/2 -translate-y-1/4"></div> {/*   ELEMENT 1   */}
+          <div className="absolute top-1/2 -translate-y-1/4">one</div> {/*   ELEMENT 1   */}
         </li>
         <li className="relative h-full w-2/6 flex justify-center">
-          <div className="absolute top-1/2 -translate-y-1/4"></div> {/*   ELEMENT 2   */}
+          <div className="absolute top-1/2 -translate-y-1/4">two</div> {/*   ELEMENT 2   */}
         </li>
         {/*   ELEMENT 3   */}
         <li className="relative flex items-end justify-center w-2/6 h-full">
           <div className="absolute z-60 w-12 h-12">
             {customerOrder.length !== 0 ? (
-              <>
+              <a href="/menu/order">
                 <span
                   className="absolute z-20 bottom-5 h-7 w-7  bg-custom-peach-dark  rounded-full 
                          left-1/2 -translate-x-1/2 grid place-items-center"
@@ -30,9 +30,9 @@ export function Heading() {
                   alt=""
                   className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-5xl"
                 />
-              </>
+              </a>
             ) : (
-              <>
+              <a href="/menu/order">
                 <Text
                   tag="p"
                   weight="bold"
@@ -45,7 +45,7 @@ export function Heading() {
                   alt=""
                   className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-5xl"
                 />
-              </>
+              </a>
             )}
           </div>
         </li>

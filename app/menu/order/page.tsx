93a3dce85ea59ea03a7d0e936aa50ef6670d0ba2
Joschka Eickhoff <jeickhoff@posteo.de>
2024-05-24 15:22:42 +0200

    update/page_order

--- app/menu/order/page.tsx
+++ app/menu/order/page.tsx
@@ -3,16 +3,19 @@
 import { Button } from "@/app/components/ui/button";
 import { OrderList } from "@/app/components/ui/orderList";
 import { LinkCo } from "@/app/components/ui/link";
+import { IoIosArrowBack } from "react-icons/io";
 
 export default function CustomerOrders() {
   return (
     <main>
-      <header className="w-full bg-slate-200 flex justify-center gap-5">
-        <LinkCo href={"/menu"}>back</LinkCo>
-        heading
+      <header className="relative w-full h-20 bg-slate-200 flex flex-col justify-center items-center">
+        <LinkCo href={"/menu"} className="absolute top-1/2 -translate-y-1/2 left-3">
+          <IoIosArrowBack />
+        </LinkCo>
+        <div className="">Your Orders</div>
       </header>
       <OrderList />
-      <footer className="w-full flex justify-center border-2">
+      <footer className="w-full flex justify-center">
         <Button className="border-2">Place Order</Button>
       </footer>
     </main>

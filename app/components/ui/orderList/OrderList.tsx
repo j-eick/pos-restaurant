22323d8931e9bd4fba5c0ca1a251f877d8412e40
Joschka Eickhoff <jeickhoff@posteo.de>
2024-05-24 15:21:43 +0200

    page/OrderList

--- app/components/ui/orderList/OrderList.tsx
+++ app/components/ui/orderList/OrderList.tsx
@@ -1,19 +1,26 @@
 // import { useCustomerOrders } from "@/app/context/CustomerOrderProvider";
 import { useEffect, useId } from "react";
+import useOrderStore from "@/app/hooks/useOrderStore";
+import { exampleOrder } from "@/app/lib/exampleOrder";
+import { Text } from "../text";
 
 export function OrderList() {
-  // const { customerOrder, setCustomerOrder } = useCustomerOrders();
-  const id = useId();
+  const orderList = useOrderStore((state) => state.orderList);
 
   useEffect(() => {
-    // console.log(customerOrder);
+    console.log(orderList);
   }, []);
 
   return (
-    <ul>
-      {/* {customerOrder.map((item) => (
-        <li key={id}>{item.title}</li>
-      ))} */}
-    </ul>
+    <main className="flex justify-center">
+      <ul className="w-4/5 flex flex-col mt-5 mb-5 align-middle gap-5  bg-red-300">
+        {exampleOrder.map((item, i) => (
+          <li key={i} className="inline-flex flex-col border-2 border-slate-400">
+            <span className="inline-block">#{item.id}</span>
+            <Text tag="p">{item.title}</Text>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }

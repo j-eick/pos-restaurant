6bc80e15861bb2f1efb586d186e82e8993006495
Joschka Eickhoff <jeickhoff@posteo.de>
2024-06-04 21:05:35 +0200

    fix (incomplete) / Link.tsx

--- app/components/ui/link/Link.tsx
+++ app/components/ui/link/Link.tsx
@@ -5,27 +5,53 @@ type LinkProp = {
   children: ReactNode;
   href: "" | "/menu" | "/order-summary";
   className?: string;
-  variant?: "" | "menuLink";
+  variant?: "" | "menuLink" | "small" | "medium";
+  blur?: "light" | "medium" | "strong";
 };
 
-export const LinkCo = ({ children, href = "", className, variant = "" }: LinkProp) => {
+export const LinkCo = ({ children, href = "", className, variant = "", blur }: LinkProp) => {
   const style = useMemo(() => {
     const hover = {
-      red: `transition-all hover:bg-red-hover`,
+      // peach: `transition-all hover:bg-custom-peach-dark`,
+      // outline outline-2 outline-offset-4 outline-slate-100
+      peach: `transition-all hover:outline hover:outline-2 hover:outline-offset-4 hover:outline-slate-100
+      active:outline active:outline-2 active:outline-offset-4 active:outline-slate-100`,
     };
 
     switch (variant) {
       case "menuLink":
-        return `px-5 py-5 bg-red rounded-xl cursor-pointer ${hover.red}`;
+        return `px-5 py-5 bg-custom-peach rounded-sm cursor-pointer ${hover.peach}`;
+
+      case "small":
+        return `px-5 py-5 bg-red rounded-xl cursor-pointer ${hover.peach}`;
+
+      case "medium":
+        return `px-5 py-5 bg-custom-peach rounded-xl cursor-pointer ${hover.peach}`;
 
       default:
         return "px-3 py-3 bg-gray-200 rounded-xl";
     }
-  }, []);
+  }, [variant]);
+
+  const bg_blur = useMemo(() => {
+    switch (blur) {
+      case "light":
+        return "backdrop-blur-sm";
+      case "medium":
+        return "backdrop-blur-md";
+      case "strong":
+        return "backdrop-blur-lg";
+
+      default:
+        return "";
+    }
+  }, [blur]);
 
   return (
-    <Link href={href} className={`${style} ${className}`}>
-      {children}
-    </Link>
+    <div className={`${bg_blur}`}>
+      <Link href={href} className={`${style} ${className}`}>
+        {children}
+      </Link>
+    </div>
   );
 };

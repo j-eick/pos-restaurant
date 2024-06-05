098bb37da3c4d05db22a96ce3f62fabad55d2d56
Joschka Eickhoff <jeickhoff@posteo.de>
2024-06-05 20:53:36 +0200

    add / shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,6 +44,7 @@ module.exports = {
       boxShadow: {
         dishCard:
           "rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px",
+        dishCard_shallow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px",
         dishPhoto:
           "rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
         dishOrder: "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",

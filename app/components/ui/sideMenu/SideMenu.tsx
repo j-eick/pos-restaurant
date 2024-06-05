66407a225eb75a5afd7ddbbb969826a723075b17
Joschka Eickhoff <jeickhoff@posteo.de>
2024-06-05 13:17:33 +0200

    update / sideMenu => color fix

--- app/components/ui/sideMenu/SideMenu.tsx
+++ app/components/ui/sideMenu/SideMenu.tsx
@@ -11,10 +11,11 @@ export const SideMenu = ({ isOpen, inView, highlight }: SideMenuProps) => {
   const show = "translate-x-0 transition-all duration-300 ease-in-out";
   const hide = "translate-x-[-100%] transition-all duration-300 ease-in-out";
 
-  const openClose = `fixed z-30 top-1/2 -translate-y-[45%] w-7/12 h-4/5
-      rounded-r-2xl pt-20 pb-20 pl-4 backdrop-blur-lg bg-opacity-60 bg-slate-300 
-      border-2 border-t-app-primary border-r-app-primary border-b-app-primary
+  const openClose = `fixed z-30 top-1/2 -translate-y-[45%] w-8/12 h-4/5
+      rounded-r-md pt-14 pb-14 pl-4 backdrop-blur-lg bg-opacity-60 bg-[#8aa0c1]
       ${isOpen ? show : hide}`;
+  // outline outline-1 outline-offset-2 outline-slate-400
+  // border-[1px] border-t-black border-r-black border-b-black
 
   return (
     <aside className={openClose}>
@@ -23,18 +24,18 @@ export const SideMenu = ({ isOpen, inView, highlight }: SideMenuProps) => {
           className="absolute top-[-20px] bottom-[-20px]
                         left-0 border-l-2 border-slate-400"
         /> */}
-        <span className="absolute w-1 h-full border-1 bg-slate-400" />
+        {/* <span className="absolute w-1 h-full border-1 bg-slate-400" /> */}
         <ul className="h-full flex flex-col gap-3 pt-2 pb-2 overflow-auto">
           {menuItems.map((item) => (
             <li key={item.id}>
               {highlight === item.title ? (
-                <div className="border-b-2 border-slate-400">
-                  <Text tag="p" size="regular" weight="semibold" className="pl-3">
-                    {item.title}
-                  </Text>
-                </div>
+                // <div className="border-b-2 border-slate-400">
+                <Text tag="p" weight="bold" className="pl-3 text-[18px] text-zinc-100">
+                  {item.title}
+                </Text>
               ) : (
-                <Text tag="p" size="regular" className="pl-3">
+                // </div>
+                <Text tag="p" size="regular" weight="semibold" className="pl-3 text-gray-600">
                   {item.title}
                 </Text>
               )}

d8c934239166b3325f66d7d03791cbb6b52c06bd
Joschka Eickhoff <jeickhoff@posteo.de>
2024-06-07 17:39:54 +0200

    added / hover.order to Link comp

--- app/components/ui/link/Link.tsx
+++ app/components/ui/link/Link.tsx
@@ -5,22 +5,27 @@ type LinkProp = {
   children: ReactNode;
   href: "" | "/menu" | "/order-summary";
   className?: string;
-  variant?: "" | "menuLink" | "small" | "medium";
+  variant?: "" | "menuLink" | "placeOrderLink" | "small" | "medium";
   blur?: "light" | "medium" | "strong";
 };
 
-export const LinkCo = ({ children, href = "", className, variant = "", blur }: LinkProp) => {
+export const LinkCo = ({ children, href = "", className, variant, blur }: LinkProp) => {
   const style = useMemo(() => {
     const hover = {
       // peach: `transition-all hover:bg-custom-peach-dark`,
       // outline outline-2 outline-offset-4 outline-slate-100
       peach: `transition-all hover:outline hover:outline-2 hover:outline-offset-4 hover:outline-slate-100
       active:outline active:outline-2 active:outline-offset-4 active:outline-slate-100`,
+      order: `transition-all bg-slate-200 hover:bg-custom-peach
+      active:outline active:outline-2 active:outline-offset-4 active:outline-slate-100`,
     };
 
     switch (variant) {
       case "menuLink":
         return `px-5 py-5 bg-custom-peach rounded-sm cursor-pointer ${hover.peach}`;
+
+      case "placeOrderLink":
+        return `px-5 py-5 rounded-sm cursor-pointer ${hover.order}`;
 
       case "small":
         return `px-5 py-5 bg-red rounded-xl cursor-pointer ${hover.peach}`;

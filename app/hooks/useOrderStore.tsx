3eada0d3a91cf05b793852dd396dbd50de214955
Joschka Eickhoff <jeickhoff@posteo.de>
2024-06-13 12:23:46 +0200

    useStore / added selectedCategory + changeCategory

--- app/hooks/useOrderStore.tsx
+++ app/hooks/useOrderStore.tsx
@@ -5,12 +5,16 @@ type OrderState = {
   orderList: DishProps[];
   addOrder: (newDish: DishProps) => void;
   clearList: () => void;
+  selectedCategory: "" | "drink" | "food" | "dessert";
+  changeCategory: (category: "" | "drink" | "food" | "dessert") => void;
 };
 
 const useOrderStore = create<OrderState>()((set) => ({
   orderList: [],
+  selectedCategory: "drink",
+  changeCategory: (category) => set(() => ({ selectedCategory: category })),
   addOrder: (newDish) => set((state) => ({ orderList: [...state.orderList, newDish] })),
-  clearList: () => set((state) => ({ orderList: [] })),
+  clearList: () => set(() => ({ orderList: [] })),
 }));
 
 export default useOrderStore;

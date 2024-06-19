efd475008a9b909c29ddb3fd58ee7cc38a1e4302
Joschka Eickhoff <jeickhoff@posteo.de>
2024-06-19 21:19:04 +0200

    Minor changes to orderPage

--- app/menu/order/page.tsx
+++ app/menu/order/page.tsx
@@ -1,33 +1,41 @@
 "use client";
 
 import { Text } from "@/app/components/ui/text";
-import { Button } from "@/app/components/ui/button";
 import { OrderList } from "@/app/components/ui/orderList";
 import { LinkCo } from "@/app/components/ui/link";
 import { IoIosArrowBack } from "react-icons/io";
-import { BsArrowLeftCircle } from "react-icons/bs";
 import { LuConciergeBell } from "react-icons/lu";
+import useOrderStore from "@/app/hooks/useOrderStore";
 
 export default function CustomerOrders() {
-  function placeOrderHandler() {
-    console.log("asd");
-  }
+  const resetOrderList = useOrderStore((state) => state.clearList);
+  const orderList = useOrderStore((state) => state.orderList);
 
-  const margin_tb = "mt-10 mb-10";
+  function placeOrderHandler() {
+    resetOrderList();
+  }
 
   return (
     <main>
-      <header className="relative w-full h-16 bg-slate-300 flex justify-center items-center">
-        <LinkCo href={"/menu"} className="absolute top-1/2 -translate-y-1/2 left-3 bg-transparent">
+      <header className="relative w-full h-16 flex justify-center items-center">
+        <LinkCo
+          href={"/menu"}
+          className="absolute top-1/2 -translate-y-1/2 left-3 
+                          flex gap-3 items-center
+                          bg-transparent"
+        >
           <IoIosArrowBack className="text-lg" />
+          back to menu
         </LinkCo>
-        <Text tag="h1" size="lg" weight="semibold">
-          wishlist
-        </Text>
       </header>
       <OrderList />
-      <div className="w-full mt-10 flex justify-center">
-        <LinkCo href={""} variant="placeOrderLink" className="flex items-center gap-3">
+      <div className="w-full mt-20 flex justify-center">
+        <LinkCo
+          href={"/complete"}
+          variant="placeOrderLink"
+          onClick={placeOrderHandler}
+          className="mb-20 flex items-center gap-3"
+        >
           <Text tag="p" weight="semibold">
             Place Order
           </Text>

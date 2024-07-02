cfdadb85c6830e15bf66dc1fd39e4b8dc8ed31ac
Joschka Eickhoff <jeickhoff@posteo.de>
2024-07-02 04:46:01 +0200

    added deatils to exampleOrders for debugging

--- app/lib/exampleOrder.tsx
+++ app/lib/exampleOrder.tsx
@@ -1,51 +1,43 @@
 export type DishProps = {
   id: string;
+  ident: string;
   title: string;
   photo: string;
-  gallery: string[];
   description: string;
   ingredients: string[];
   allergens: string[];
   altText: string;
   price: string;
-  type: "food" | "beverage";
+  category: "drink" | "food" | "dessert";
+  selected: boolean;
 };
 
 export const exampleOrder: DishProps[] = [
   {
-    id: "3",
-    title: "Spaghetti Carbonara",
-    photo:
-      "https://production-media.gousto.co.uk/cms/mood-image/2338---Creamy-Spaghetti-Carbonara-copy-1675769448672.jpg",
-    gallery: [
-      "https://placeholder.com/100",
-      "https://placeholder.com/100",
-      "https://placeholder.com/100",
-      "https://placeholder.com/100",
-    ],
-    description: "Rich and creamy spaghetti with pancetta and a touch of egg.",
-    ingredients: ["Spaghetti", "egg", "pancetta", "Parmesan cheese", "black pepper"],
-    allergens: ["Gluten", "Egg", "Dairy"],
-    altText: "Spaghetti Carbonara",
-    price: "15.00",
-    type: "food",
+    id: "5",
+    ident: "beef-burger",
+    title: "Beef Burger",
+    photo: "https://staticcookist.akamaized.net/wp-content/uploads/sites/22/2021/09/beef-burger.jpg",
+    description: "Juicy beef burger with lettuce, tomato, and secret sauce.",
+    ingredients: ["Beef patty", "lettuce", "tomato", "burger bun", "secret sauce"],
+    allergens: ["Gluten"],
+    altText: "Beef Burger",
+    price: "11.95",
+    category: "food",
+    selected: false,
   },
   {
-    id: "7",
-    title: "Fish and Chips",
+    id: "6",
+    ident: "chicken-tikka-masala",
+    title: "Chicken Tikka Masala",
     photo:
-      "https://popmenucloud.com/cdn-cgi/image/width%3D1200%2Cheight%3D1200%2Cfit%3Dscale-down%2Cformat%3Dauto%2Cquality%3D60/qjiendmr/01ee296e-a5d7-4788-8323-24befd50b1db.jpg",
-    gallery: [
-      "https://placeholder.com/100",
-      "https://placeholder.com/100",
-      "https://placeholder.com/100",
-      "https://placeholder.com/100",
-    ],
-    description: "Classic British beer-battered fish with crispy fries.",
-    ingredients: ["Fish", "flour", "beer", "potatoes", "tartar sauce"],
-    allergens: ["Fish", "Gluten"],
-    altText: "Fish and Chips",
-    price: "12.00",
-    type: "food",
+      "https://images.squarespace-cdn.com/content/v1/63d0a16cfad8c1759df2fe31/49cce2fb-0fd7-427e-b0f9-80aa32541cb2/GCBC16_EP30_Chicken+Tikka+Masala_1L0A4580.jpg",
+    description: "Grilled chicken in a spicy and creamy masala sauce.",
+    ingredients: ["Chicken", "yogurt", "tomatoes", "onion", "masala spices"],
+    allergens: ["Dairy"],
+    altText: "Chicken Tikka Masala",
+    price: "13.50",
+    category: "food",
+    selected: false,
   },
 ];

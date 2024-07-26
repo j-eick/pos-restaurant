f8e9f7f8701337ce832837789113ffe3fb9b8d6b
Joschka Eickhoff <jeickhoff@posteo.de>
2024-07-26 02:32:07 +0200

    fix / using selectedItems[] for Bowl Icon

--- app/components/ui/icon/Bowl.tsx
+++ app/components/ui/icon/Bowl.tsx
@@ -1,23 +1,22 @@
 import useOrderStore from "@/app/hooks/useOrderStore";
 import Link from "next/link";
-import { Text } from "../text";
 
 export function Bowl() {
-  const allOrders = useOrderStore((state) => state.orderList);
+  const store = useOrderStore();
 
   return (
     <div
       className={`relative w-12
                   sm:w-16`}
     >
-      {allOrders.length !== 0 ? (
+      {store.selectedItems.length !== 0 ? (
         <Link href="/menu/order">
           <div
             className={`absolute z-20 bottom-6 h-7 w-7 bg-custom-peach-dark rounded-full 
               left-1/2 -translate-x-1/2 grid place-items-center
               sm:h-8 sm:w-8 sm:text-lg sm:top-0 sm:animate-scale`}
           >
-            {allOrders.length}
+            {store.selectedItems.length}
           </div>
           <img src="bowl.svg" alt="" />
         </Link>
@@ -28,7 +27,7 @@ export function Bowl() {
                        bg-slate-500 rounded-full text-slate-300 
                          sm:h-8 sm:w-8 sm:text-lg sm:top-0`}
           >
-            {allOrders.length}
+            {store.selectedItems.length}
           </div>
           {/* <Text
             tag="p"
